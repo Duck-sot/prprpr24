9028331e2a104294390666ba5280f7ed110ea234
Duck-sot <raja07001@utb.vaxjo.se>
2025-01-15 11:20:46 +0100

    carti

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+
+static void QS(int[]arr, int n){
+    int q, s , tem;
+    bool qsed;
+    for(q = 0; q< n-1;q++){
+        qsed = false;
+        for(s=0; s < n; s++){
+            if(arr[s] > arr[s+1]){
+                tem = arr[s];
+                arr[s]=arr[s +1];
+                arr[s + 1] = tem;
+                qsed = true; 
+            }
+        }
+        if(qsed = false){
+            break;
+        }
+    } 
+
+} 
